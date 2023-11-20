faedcdae7ba78d62fb1f27bd9aa9a7a100cb6e61
mahmoud <information.mahmoud@gmail.com>
2023-11-20 22:39:57 +0200

    best merge

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -15,39 +15,34 @@ void merge(int *array, int *temp, size_t left_size, int *right,
 {
 	size_t i = 0, j = 0, k = 0;
 
+	/* Copy the left subarray into the temp array*/
 	for (i = 0; i < left_size; i++)
-	{
 		temp[i] = array[i];
-	}
 
-	i = 0;
+	i = 0; /* Reset i to start from the beginning of the temp (left) array*/
 
 	printf("Merging...\n[left]: ");
 	print_array(temp, left_size);
 	printf("[right]: ");
 	print_array(right, right_size);
 
+	/*Merge the two subarrays into the original array*/
 	while (i < left_size && j < right_size)
 	{
 		if (temp[i] <= right[j])
-		{
 			array[k++] = temp[i++];
-		}
+
 		else
-		{
 			array[k++] = right[j++];
-		}
 	}
 
+	/*Copy any remaining elements from the left subarray*/
 	while (i < left_size)
-	{
 		array[k++] = temp[i++];
-	}
 
+	/*Copy any remaining elements from the right subarray*/
 	while (j < right_size)
-	{
 		array[k++] = right[j++];
-	}
 
 	printf("[Done]: ");
 	print_array(array, k);
